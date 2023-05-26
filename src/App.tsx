00f01b3689379ac35ddb30c82a6dcdf2238fecf3
MaymoonaAlBoloshi <maymoona.al.boloshi@gmail.com>
2023-05-26 20:45:43 +0400

    bring back cto events, overwriten by mistake, add game component

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,9 @@
-import { useRef, useState } from 'react'
-import { Piller } from './components/piller'
+import Game from './screens/game'
 
 function App() {
-  const cardRef = useRef<HTMLDivElement>(null)
-  const [employeesScore, setEmployeesScore] = useState(50)
-  const [customersScore, setCustomersScore] = useState(50)
-  const [investorsScore, setInvestorsScore] = useState(50)
-  const [competetorsScore, setCompetetorsScore] = useState(50)
-
-  const handleMouseEnter = (isRight: boolean) => {
-    if (cardRef.current) {
-      cardRef.current.style.transform = isRight ? 'rotate(25deg)' : 'rotate(-25deg)';
-      cardRef.current.style.boxShadow = isRight ? '0 0 40px #059669' : '0 0 40px #991b1b';
-    }
-  };
-
-  const handleMouseLeave = () => {
-    if (cardRef.current) {
-      cardRef.current.style.transform = 'rotate(0deg)';
-      cardRef.current.style.boxShadow = 'none';
-      cardRef.current.style.opacity = '1';
-    }
-  };
-
-
-  const handleClick = () => {
-    if (cardRef.current) {
-      cardRef.current.style.opacity = '0.2';
-      cardRef.current.style.transform = 'scale(0.8)';
-    }
-  }
   return (
-    <div className="flex flex-col w-screen h-screen">
-      <header className=" flex justify-between px-44 basis-1/12 bg-black text-white text-md p-4">
-        <Piller color="emerald" score={employeesScore} />
-        <Piller color="rose" score={investorsScore} />
-        <Piller color="yellow" score={competetorsScore} />
-        <Piller color="violet" score={customersScore} />
-      </header>
-      <main className="flex justify-center items-center gap-12 basis-11/12 bg-black text-md text-white p-4">
-        <button onMouseEnter={() => handleMouseEnter(false)}
-          onClick={handleClick}
-          onMouseLeave={handleMouseLeave}
-          className="border-2 border-rose-600 px-2 text-rose-300 py-1 hover:px-3 duration-700 hover:py-2">
-          decline
-        </button>
-        <div ref={cardRef} className="flex flex-col items-center gap-2 flex-row w-64 h-96 border-2 border-yellow-50 transition-all duration-700">
-          <p className="text-yellow-200 w-full p-4 text-center">Character</p>
-          <img className="w-36" src="https://via.placeholder.com/64" alt="character" />
-          <p className="italic text-sm text-center text-yellow-50">
-            <span className="text-yellow-200 text-2xl">"</span>
-            lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            <span className="text-yellow-200 text-2xl m-b-2">"</span>
-          </p>
-        </div>
-        <button onMouseEnter={() => handleMouseEnter(true)}
-          onClick={handleClick}
-          onMouseLeave={handleMouseLeave}
-          className="border-2 border-emerald-600 px-2 text-emerald-300  py-1 hover:px-3 duration-700 hover:py-2">
-          Accept
-        </button>
-      </main>
-    </div >
+    <Game />
   )
 }
 
 export default App
-//TODO: add audio button with icon
-//TODO: add character events
---
+++ src/data/cmo-events.ts
@@ -0,0 +1,94 @@
+import { CharacterEvent } from "../types//character-event";
+import { CharacterType } from "../types/character";
+import { PillerType } from "../types/pillers";
+
+
+export const CMOEvents: CharacterEvent = {
+  character: CharacterType.CMO,
+  events: [
+    {
+      id: 1,
+      event: "Our social media presence is lacking. Should we invest more in our online marketing?",
+      AcceptEffects: [
+        {
+          pillerName: PillerType.CUSTOMER,
+          effect: 3 // greater online presence can attract more customers
+        },
+        {
+          pillerName: PillerType.INVESTOR,
+          effect: -2 // more marketing expenses might concern investors
+        },
+        {
+          pillerName: PillerType.COMPETITOR,
+          effect: 2 // strong online presence can give a competitive edge
+        }
+      ],
+      RejectEffects: [
+        {
+          pillerName: PillerType.CUSTOMER,
+          effect: -2 // weak online presence can result in losing potential customers
+        },
+        {
+          pillerName: PillerType.COMPETITOR,
+          effect: -2 // competitors with stronger online presence might get ahead
+        }
+      ]
+    },
+    {
+      id: 2,
+      event: "We have an opportunity to sponsor a major event. Should we go for it?",
+      AcceptEffects: [
+        {
+          pillerName: PillerType.CUSTOMER,
+          effect: 3 // sponsoring events can increase brand visibility and attract more customers
+        },
+        {
+          pillerName: PillerType.INVESTOR,
+          effect: -3 // the high cost of sponsorship might worry investors
+        },
+        {
+          pillerName: PillerType.COMPETITOR,
+          effect: 1 // sponsorship can give a competitive edge
+        }
+      ],
+      RejectEffects: [
+        {
+          pillerName: PillerType.CUSTOMER,
+          effect: -1 // missed sponsorship opportunity can lead to less visibility
+        },
+        {
+          pillerName: PillerType.COMPETITOR,
+          effect: -1 // competitors might take the opportunity and get ahead
+        }
+      ]
+    },
+    {
+      id: 3,
+      event: "Our competitor launched a negative ad campaign against us. Should we retaliate with our own campaign?",
+      AcceptEffects: [
+        {
+          pillerName: PillerType.CUSTOMER,
+          effect: 1 // defending the company might retain some customers
+        },
+        {
+          pillerName: PillerType.INVESTOR,
+          effect: -1 // might worry investors due to potential escalation
+        },
+        {
+          pillerName: PillerType.COMPETITOR,
+          effect: -1 // retaliation might escalate the situation with competitors
+        }
+      ],
+      RejectEffects: [
+        {
+          pillerName: PillerType.CUSTOMER,
+          effect: -2 // ignoring the campaign might lead to loss of customer trust
+        },
+        {
+          pillerName: PillerType.COMPETITOR,
+          effect: 1 // staying above the fray might be seen as a positive trait
+        }
+      ]
+    }
+  ]
+};
